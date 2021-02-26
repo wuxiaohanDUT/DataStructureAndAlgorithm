5c8ba865479c9f401781cf531645aa89ed522cea
wuxiaohanDUT <2658343834@qq.com>
2021-02-26 22:16:09 +0800

    BlockQueue

--- src/BlockQueue/BlockQueue.java
+++ src/BlockQueue/BlockQueue.java
@@ -5,13 +5,16 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class BlockQueue<T> {
 
+    //最大容量和队列数组
     private int capacity;
     private Object[] queue;
 
+    //锁和等待条件
     private ReentrantLock lock;
     private Condition empty;
     private Condition full;
 
+    //添加、删除元素的下标 当前队列的大小
     private int removeIndex;
     private int index;
     private volatile int size;
@@ -31,43 +34,77 @@ public class BlockQueue<T> {
         full=lock.newCondition();
     }
 
+    /**
+     * 插入元素,保证线程安全
+     * @param element
+     * @throws InterruptedException
+     */
     public void push(T element) throws InterruptedException {
         lock.lockInterruptibly();
         try {
-            while(size==capacity){
-                full.wait();
+            try{
+                while (size==capacity){
+                    full.await();
+                }
+            }catch (InterruptedException e){
+                full.signal();//线程中段，节点放弃等待，唤醒之后的线程
+                throw e;
             }
-            ++size;
-            queue[index++]=element;
-            if(index==capacity){
-                index=0;
-            }
-        }catch (InterruptedException e){
-            full.signal();
-            throw e;
+            insert(element);
         }finally {
             lock.unlock();
         }
     }
 
+    /**
+     * 插入元素，并且唤醒empty条件队列中的等待线程
+     * @param element
+     */
+    private void insert(T element){
+        ++size;
+        queue[index++]=element;
+        if(index==capacity){
+            index=0;
+        }
+        empty.signal();
+    }
+
+    /**
+     * 移除元素，保证线程安全
+     * @return
+     * @throws InterruptedException
+     */
     public T pop() throws InterruptedException{
         lock.lockInterruptibly();
-        Object ans=null;
+        T ans;
         try {
-            while(size==0){
-                empty.wait();
+            try{
+                while (size==0){
+                    empty.await();
+                }
+            }catch (InterruptedException e){
+                empty.signal();
+                throw e;
             }
-            --size;
-            ans=queue[removeIndex++];
-            if(removeIndex==capacity){
-                removeIndex=0;
-            }
-        }catch (InterruptedException e){
-            empty.signal();
-            throw e;
+            ans=remove();
         }finally {
             lock.unlock();
         }
-        return (T)ans;
+        return ans;
+    }
+
+    /**
+     * 移除元素
+     * @return
+     */
+    private T remove(){
+        size--;
+        T ans=(T)queue[removeIndex];
+        ++removeIndex;
+        if(removeIndex==capacity){
+            removeIndex=0;
+        }
+        full.signal();
+        return ans;
     }
 }
