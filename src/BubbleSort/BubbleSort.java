2ae24e4854143ee1368ff2e9ccfcd5a4c9b20898
wuxiaohanDUT <2658343834@qq.com>
2021-02-28 13:41:20 +0800

    BubbleSort

---
+++ src/BubbleSort/BubbleSort.java
@@ -0,0 +1,34 @@
+package BubbleSort;
+
+import HeapSort.HeapSort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleSort {
+
+    public void sort(int[] nums,int begin,int end){
+        for(int i=0;i<end-begin;++i){
+            for(int j=begin;j<end-1-i;++j){
+                if(nums[j]>nums[j+1]){
+                    swap(nums,j,j+1);
+                }
+            }
+        }
+    }
+
+    private void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public static void main(String[] argc){
+        int randomLen=new Random().nextInt(20);
+        int[] nums=new int[randomLen];
+        for(int i=0;i<randomLen;++i){
+            nums[i]=new Random().nextInt(100);
+        }
+        new BubbleSort().sort(nums,0,randomLen);
+        System.out.println(Arrays.toString(nums));
+    }
+}
